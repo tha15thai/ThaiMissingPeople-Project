88edca2016cb001f1f2f2066f6b759d68e6b1be4
Jutinut-BBBOMB <kirikaya17@gmail.com>
2026-05-07 18:45:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,14 +15,10 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",  
-        "http://127.0.0.1:5501",   # เผื่อเพื่อนรันผ่าน Live Server
-        "*"                        # เผื่อเว็บอื่นๆ
-    ],
+    allow_origins=[""],
     allow_credentials=False,
-    allow_methods=["*"], # ยอมรับทุก Method (POST, GET, etc.)
-    allow_headers=["*"], # ยอมรับทุก Header
+    allow_methods=[""], # ยอมรับทุก Method (POST, GET, etc.)
+    allow_headers=[""], # ยอมรับทุก Header
 )
 # ==========================================
 # Regex
